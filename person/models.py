65cb511e256f5c98063b0c12cab3769d68f3d8f8
DelaramGhaemmaghami <delaramsadatghaemmaghami1997@gmail.com>
2021-11-28 19:08:28 +0330

    Online shop models phone regex

--- person/models.py
+++ person/models.py
@@ -1,19 +1,15 @@
 import os.path
-import re
 
-from django.core.exceptions import ValidationError
 from django.db import models
 from colorfield.fields import ColorField
+from django.core.validators import RegexValidator
+
+PHONE_REGEX = RegexValidator("09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}")
 
 
 class Store(models.Model):
-    def validate_phone(self):
-        pattern = re.compile("09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}")
-        if not pattern.match(str(self.phone)):
-            raise ValidationError(f"{self.phone} is not a valid phone number!")
-
     name = models.CharField(max_length=100, blank=False, unique=True)
-    phone = models.CharField(max_length=15, validators=[validate_phone])
+    phone = models.CharField(max_length=15, validators=[PHONE_REGEX])
     location = models.CharField(max_length=200)
     about_store = models.TextField()
 
